6f7960c739d4d8f1810e359da18eb53c97da7556
zaclimon <isaacpateau05@gmail.com>
2016-09-09 08:13:32 -0400

    full_mako: Adapt the model to "Nexus 4"

--- full_mako.mk
+++ full_mako.mk
@@ -33,7 +33,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 PRODUCT_NAME := full_mako
 PRODUCT_DEVICE := mako
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Mako
+PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
